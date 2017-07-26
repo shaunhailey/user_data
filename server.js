f07061f04b2f8329367eae6ddd2225ad3a647331
shaunhailey <ediblelysol@gmail.com>
2017-07-26 17:27:54 -0400

    semi-working

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mustacheExpress = require('mustache-express')
+const app = express()
+
+const userDirectory = require('./data')
+
+app.use(express.static('public'))
+
+app.engine('mst', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mst')
+
+app.get('/info/:id', (request, response) => {
+  const requestID = parseInt(request.params.id)
+  const foundUser = userDirectory.users.find(user => user.id === requestId)
+  response.render('info', foundUser)
+})
+
+app.get('/', (request, response) => {
+  response.render('home', userDirectory)
+})
+
+app.listen(3000, () => {
+  console.log('Hooray, our app is listening on port 3000')
+})
